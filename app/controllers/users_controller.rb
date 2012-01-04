311b3fbf613f4616dc845be9c5ae2ef44398bd85
Adam Miribyan <adam.miribyan@gmail.com>
2012-01-04 11:46:30 +0300

    populate filtered lawyers without ajax

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,28 @@ class UsersController < ApplicationController
     if current_user and current_user.is_lawyer?
       redirect_to users_path(:t=>'l')
     end
-    @lawyers = Lawyer.approved_lawyers
+
+    # Obtain lawyers according to sent GET params
+    state_id = params[:select_state].to_i
+    pa_id = params[:select_pa].to_i
+    sp_id = params[:select_sp].to_i
+    @lawyers = []
+    @state_lawyers = []
+    if state_id == 0
+      @state_lawyers = Lawyer.approved_lawyers
+    else
+      @state_lawyers = State.find(state_id).lawyers.approved_lawyers
+    end
+    if pa_id == 0
+      @lawyers = @state_lawyers
+    else
+      if sp_id == 0
+        @pa_lawyers = PracticeArea.find(pa_id).lawyers.approved_lawyers
+      else
+        @pa_lawyers = PracticeArea.find(sp_id).lawyers.approved_lawyers
+      end
+      @lawyers = @state_lawyers & @pa_lawyers
+    end
 
     # Filtering params from the next home page form
     state_id = params[:select_state].to_i
