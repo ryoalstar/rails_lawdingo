1f0af821f3f541f833d92fbce7dbea79b0defdd0
Adam Miribyan <adam.miribyan@gmail.com>
2012-01-08 06:32:37 +0300

    lawyer's practice areas listing on homepgae

--- app/helpers/lawyers_helper.rb
+++ app/helpers/lawyers_helper.rb
@@ -1,4 +1,14 @@
 module LawyersHelper
+  def practice_areas_listing(lawyer)
+    areas = lawyer.practice_areas.parent_practice_areas unless lawyer.practice_areas.blank?
+    areas_names = areas.map do |area|
+      area.name.downcase
+    end
+
+    last_area_name = areas_names.pop
+    areas_names.empty? ? last_area_name : "#{areas_names.join(', ')} and #{last_area_name}"
+  end
+
   def start_or_schedule_button(lawyer)
     if lawyer.is_online? && !lawyer.is_busy
       if logged_in?
