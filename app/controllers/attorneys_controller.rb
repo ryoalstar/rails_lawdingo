c1a2352f0fbd95e9dc84b4a0d44059c8e90effb4
Adam Miribyan <adam.miribyan@gmail.com>
2012-02-08 17:21:46 +0300

    don't show unapproved lawyers profiles

--- app/controllers/attorneys_controller.rb
+++ app/controllers/attorneys_controller.rb
@@ -1,6 +1,14 @@
 class AttorneysController < ApplicationController
+  before_filter :check_approval, only: :show
+
   def show
     @attorney = Lawyer.find(params[:id])
   end
+
+  private
+
+    def check_approval
+      redirect_to lawyers_path unless Lawyer.find(params[:id]).is_approved
+    end
 end
 
