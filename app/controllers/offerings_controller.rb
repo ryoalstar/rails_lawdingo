c2fff74d0fbbfb597a7475e585f66f659cc45441
Adam Miribyan <adam.miribyan@gmail.com>
2012-02-10 10:29:20 +0300

    authorization for offerings#index

--- app/controllers/offerings_controller.rb
+++ app/controllers/offerings_controller.rb
@@ -1,4 +1,6 @@
 class OfferingsController < ApplicationController
+  before_filter :authenticate, :ensure_self_account
+
   def index
     @lawyer = User.find(params[:user_id])
     @offering = @lawyer.offerings.new
@@ -14,5 +16,15 @@ class OfferingsController < ApplicationController
       redirect_to user_offerings_path, notice: "Error saving an offering."
     end
   end
+
+  private
+
+    def ensure_self_account
+      @user = User.find(params[:user_id])
+      
+      unless logged_in? and ((@user.id == current_user.id) or logged_in_admin?)
+        redirect_to root_path, notice: "Access denied."
+      end
+    end
 end
 
