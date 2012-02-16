ca6c26fe0ed26958a02db2012207e32fac873a8f
Adam Miribyan <adam.miribyan@gmail.com>
2012-02-16 07:06:42 +0300

    added practice_area_id to offerings table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120212223934) do
+ActiveRecord::Schema.define(:version => 20120216040244) do
 
   create_table "app_parameters", :force => true do |t|
     t.string   "name"
@@ -107,6 +107,7 @@ ActiveRecord::Schema.define(:version => 20120212223934) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.integer  "practice_area_id"
   end
 
   create_table "pages", :force => true do |t|
