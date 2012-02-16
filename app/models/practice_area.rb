da950b911749734bd8cdf62152f3d4524d472e08
Adam Miribyan <adam.miribyan@gmail.com>
2012-02-16 08:08:14 +0300

    callback for offering model to set the practice area when offer saved

--- app/models/practice_area.rb
+++ app/models/practice_area.rb
@@ -3,9 +3,9 @@ class PracticeArea < ActiveRecord::Base
   belongs_to :main_area, :class_name => "PracticeArea", :foreign_key => "parent_id"
   has_many :expert_areas
   has_many :lawyers, :through => :expert_areas
+  has_many :offerings
 
   scope :parent_practice_areas, lambda { where(:parent_id => nil) }
   scope :child_practice_areas, lambda { where("parent_id is not null") }
-
 end
 
