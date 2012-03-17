c2c4cc7d6b8da77ea3d028e4f8d19d955c417d8e
Adam Miribyan <adam.miribyan@gmail.com>
2012-03-18 01:41:41 +0300

    change personal_tagline column type to text

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120317182047) do
+ActiveRecord::Schema.define(:version => 20120317223156) do
 
   create_table "app_parameters", :force => true do |t|
     t.string   "name"
@@ -176,7 +176,7 @@ ActiveRecord::Schema.define(:version => 20120317182047) do
     t.string   "photo_content_type"
     t.integer  "photo_file_size"
     t.datetime "photo_updated_at"
-    t.string   "personal_tagline"
+    t.text     "personal_tagline"
     t.string   "bar_ids"
     t.boolean  "has_payment_info",           :default => false
     t.string   "peer_id",                    :default => "0"
