413fb0545e74d6fbbe60b80a0c2789e951ce7792
Adam Miribyan <adam.miribyan@gmail.com>
2012-03-17 21:37:35 +0300

    years practicing law added to lawyer profile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120310144219) do
+ActiveRecord::Schema.define(:version => 20120317182047) do
 
   create_table "app_parameters", :force => true do |t|
     t.string   "name"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(:version => 20120310144219) do
     t.datetime "end_date"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "conversation_id"
   end
 
   create_table "card_details", :force => true do |t|
@@ -187,6 +188,7 @@ ActiveRecord::Schema.define(:version => 20120310144219) do
     t.string   "password_reset_token"
     t.datetime "password_reset_sent_at"
     t.integer  "school_id"
+    t.integer  "license_year"
   end
 
 end
