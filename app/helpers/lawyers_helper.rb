5c8356d4b1c7b1801ced648281e89d73a465b2ca
Rolon <i.podoinitsin@itima.ru>
2012-04-24 18:59:06 +0400

    profile bug fixes

--- app/helpers/lawyers_helper.rb
+++ app/helpers/lawyers_helper.rb
@@ -143,10 +143,10 @@ module LawyersHelper
       link_to "start live consultation", phonecall_path(:id => lawyer.id), :id => "start_phone_session_button", :data => { :l_id => lawyer.id, :fullname => lawyer.first_name }, :class => "button gray"
     else
       # link_to "start phone consultation", "#paid_schedule_session", :id => "start_phone_session_button", :data => { :attorneyid => lawyer.id, :fcd => lawyer.free_consultation_duration, :lrate => lawyer.rate, :fullname => lawyer.first_name },:class => "dialog-opener button gray"
-      link_to "start live consultation", call_payment_path(lawyer.id), :id => "start_phone_session_button", :class => "button gray"
+      link_to "start live consultation", call_payment_path(lawyer.id), :id => "start_phone_session_button", :class => ""
     end
   else
-    link_to 'start live consultation', new_user_path(ut: 0, notice: true, return_path: phonecall_path(:id => lawyer.id)), :class => "button gray"
+    link_to 'start live consultation', new_user_path(ut: 0, notice: true, return_path: phonecall_path(:id => lawyer.id)), :class => ""
   end
  end
 
