94fb93b5159232b2fa3562d6f735e65b75281aad
Rolon <i.podoinitsin@itima.ru>
2012-05-04 11:27:38 +0400

    bug with carousel fixed

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -204,7 +204,7 @@ $(function(){
         function goto_image( i ){
             self.current_image = i;
             self.find('.carousel-image img').attr('src', self.images[self.current_image]['url']);
-						self.find('.carousel-image a').attr('href', self.images[self.current_image]['href']);
+						self.find('.carousel-image a.profile_link').attr('href', self.images[self.current_image]['href']);
             self.find('.carousel-image img').attr('alt', self.images[self.current_image]['title']);
             self.find('.carousel-description h4').html( self.images[self.current_image]['title'] );
             self.find('.carousel-description p.desc').html( self.images[self.current_image]['description'] );
