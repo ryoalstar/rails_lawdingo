3255ae886141c2c777307d43b1faf96c7ebad919
Your Name <you@example.com>
2012-06-04 19:29:16 +0400

    debugging yelp reviews in the carousel

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -120,7 +120,7 @@ class SearchController < ApplicationController
          if lawyer.yelp_business_id.present? && !!lawyer.yelp[:reviews]
            Rails.logger.info lawyer.yelp[:review_count].inspect
            images_hash["reviews"] = "#{lawyer.yelp[:review_count]} <a href='http://www.yelp.com'><img src='/assets/miniMapLogo.png' /></a> reviews".html_safe
-           images_hash["link_reviews"] = "<a href='/attorneys/#{lawyer.id}/#{CGI::escape(lawyer.full_name)}#reviews' class = 'reviews'><span class = 'number_rev'></span></a>"
+           images_hash["link_reviews"] = "<a href='/attorneys/#{lawyer.id}/#{CGI::escape(lawyer.full_name)}#reviews' class = 'yelp_reviews'><span class = 'number_rev'></span></a>"
            images_hash["rating"] = "<img src='#{lawyer.yelp[:rating_img_url]}' />"
          else
            images_hash["reviews"] = "#{lawyer.reviews.count} reviews"
