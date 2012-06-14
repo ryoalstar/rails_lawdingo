808f136de1f209fce15c8ed85fd3b8f530a30084
achilov <a.achilov@itima.ru>
2012-06-15 00:27:30 +0400

    hot fix offering - state name bugs

--- app/models/offering.rb
+++ app/models/offering.rb
@@ -5,13 +5,22 @@ class Offering < ActiveRecord::Base
     :touch => true
 
     
-    #solr index
+   #solr index
     searchable do
      text :description 
      text :name 
-     text :practice_area 
+     text :practice_area do
+       practice_area.name if !!practice_area
+     end
      text :user
+     text :state_name do
+       get_state_name
+     end
     end 
+    
+    def get_state_name
+      Lawyer.find(self.user.id).states.map(&:name)*","
+    end
        
     def reindex!
        Sunspot.index!(self)
