c001dff74ee1a76e132fdace1f3080613b907dab
Sergey Chernyakov <chernyakov.sergey@gmail.com>
2012-08-15 19:44:19 +0300

    issue #34424189

--- app/mailers/appointment_mailer.rb
+++ app/mailers/appointment_mailer.rb
@@ -1,15 +1,13 @@
 class AppointmentMailer < ApplicationMailer
 
-  default :from => "appointments@lawdingo.com"
+  default :from => "appointments@lawdingo.com",
+    :bcc => ["offline@lawdingo.com", "nikhil.nirmel@gmail.com", "info@lawdingo.com"]
 
   def appointment_created(appointment)
     @appointment = appointment
     mail(
-      # :to => appointment.attorney_email,
-      :to => ["chernyakov.sergey@gmail.com"],
-      :subject => "Client Appointment Request on Lawdingo.",
-      :bcc => ["dp_infoatom@mail.ru"]
-      #:bcc => ["offline@lawdingo.com", "nikhil.nirmel@gmail.com", "info@lawdingo.com"]
+      :to => appointment.attorney_email,
+      :subject => "Client Appointment Request on Lawdingo."
     )
   end
 end
