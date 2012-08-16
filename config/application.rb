d19826efab864919d9ff27f2ae665070c8c6a9c4
Sergey Chernyakov <chernyakov.sergey@gmail.com>
2012-08-16 20:48:51 +0300

    issue #34464717 IP-based state detection not working consistently

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ module Lawdingo
 
     # Custom directories with classes and modules you want to be autoloadable.
     config.autoload_paths += %W(#{config.root}/app/decorators)
+    config.autoload_paths += %W(#{config.root}/lib)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
@@ -51,6 +52,6 @@ module Lawdingo
       g.form_builder :simple_form
       g.template_engine :haml
     end
-    
+
   end
 end
