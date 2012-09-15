7e05b8bb500320c16a2a2d6fd79e4c230397a8e4
Sergey Chernyakov <chernyakov.sergey@gmail.com>
2012-09-15 13:24:28 +0300

    issue #36102601 Major site error Lawdingo.com/lawyers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Lawdingo::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -39,7 +39,7 @@ Lawdingo::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( payment.js )
+  config.assets.precompile += %w( payment.js realtime_checker.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
