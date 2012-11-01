7c04a2ba483fde82b5ad3e0c7350157016e27bc5
Sergey Chernyakov <chernyakov.sergey@gmail.com>
2012-11-01 10:24:27 +0200

    issue #38777759 Lawyer subscription page payment doesn't work.

--- app/controllers/stripes_controller.rb
+++ app/controllers/stripes_controller.rb
@@ -9,10 +9,10 @@ class StripesController < ApplicationController
 
 	def create
 		respond_to do |format|
-			if @lawyer.subscribed? && params[:coupon].empty?
+			if @lawyer.subscribed? # && params[:coupon].empty?
 				format.html { redirect_to user_offerings_path(@lawyer, :ft => true), :error => "You are subscribed already!" }
         format.json { render json: @lawyer, status: :ok }
-      elsif params[:card] && @lawyer.create_stripe_card(params[:card]) && @lawyer.apply_coupon(params[:coupon]) # && @lawyer.subscribe! l
+      elsif params[:card] && @lawyer.create_stripe_card(params[:card]) # && @lawyer.apply_coupon(params[:coupon]) # && @lawyer.subscribe! l
         format.html { redirect_to user_offerings_path(@lawyer, :ft => true), :notice => "Thank you for subscribing!" }
         format.json { render json: @lawyer, status: :created, location: @lawyer } 
       elsif params[:coupon].present? && @lawyer.apply_coupon(params[:coupon])
